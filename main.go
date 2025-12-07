54ab860213c9d87b7f737dbab63eee1ea89e68c4
VictorDMoura <mouramvictor@gmail.com>
2025-12-07 18:43:49 -0300

    fix: update ball positions and increase max speed; add restart functionality

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -19,13 +20,12 @@ const (
 	Rows            = ScreenHeight / TileSize
 	InitialSpeed    = 4.0
 	SpeedMultiplier = 1.1
-	MaxSpeed        = 15.0
+	MaxSpeed        = 20.0
 )
 
 var (
 	ColorDay   = color.RGBA{R: 200, G: 200, B: 200, A: 255}
 	ColorNight = color.RGBA{R: 50, G: 50, B: 50, A: 255}
-	ColorBall  = color.RGBA{R: 255, G: 0, B: 0, A: 255}
 )
 
 type Ball struct {
@@ -39,8 +39,9 @@ type Game struct {
 	Ball []*Ball
 }
 
-func NewGame() *Game {
-	g := &Game{}
+func (g *Game) Init() {
+	g.Ball = nil
+
 	rand.Seed(time.Now().UnixNano())
 
 	for x := 0; x < Cols; x++ {
@@ -61,21 +62,29 @@ func NewGame() *Game {
 	}
 
 	g.Ball = append(g.Ball, &Ball{
-		X: 200, Y: 300,
+		X: 200, Y: float64(ScreenHeight) / 2,
 		DX: randomDir(), DY: randomDir(),
 		Color: ColorNight,
 	})
 
 	g.Ball = append(g.Ball, &Ball{
-		X: 600, Y: 300,
+		X: 600, Y: float64(ScreenHeight) / 2,
 		DX: randomDir(), DY: randomDir(),
 		Color: ColorDay,
 	})
+}
 
+func NewGame() *Game {
+	g := &Game{}
+	g.Init()
 	return g
 }
 
 func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyR) {
+		g.Init()
+		return nil
+	}
 
 	for _, b := range g.Ball {
 		b.X += b.DX
@@ -100,12 +109,12 @@ func (g *Game) Update() error {
 				b.Y = ScreenHeight
 			}
 		}
+
 		gridX := int(b.X / TileSize)
 		gridY := int(b.Y / TileSize)
 
 		if gridX >= 0 && gridX < Cols && gridY >= 0 && gridY < Rows {
 			if g.Grid[gridX][gridY] == b.Color {
-
 				if b.Color == ColorDay {
 					g.Grid[gridX][gridY] = ColorNight
 				} else {
@@ -132,19 +141,20 @@ func (g *Game) Update() error {
 				b.DY = -b.DY
 			}
 		}
+	}
 
-		dayCount, nightCount := 0, 0
-		for x := 0; x < Cols; x++ {
-			for y := 0; y < Rows; y++ {
-				if g.Grid[x][y] == ColorDay {
-					dayCount++
-				} else {
-					nightCount++
-				}
+	dayCount, nightCount := 0, 0
+	for x := 0; x < Cols; x++ {
+		for y := 0; y < Rows; y++ {
+			if g.Grid[x][y] == ColorDay {
+				dayCount++
+			} else {
+				nightCount++
 			}
 		}
-		ebiten.SetWindowTitle(fmt.Sprintf("Pong Wars | Day: %d | Night: %d", dayCount, nightCount))
 	}
+	ebiten.SetWindowTitle(fmt.Sprintf("Pong Wars | Day: %d | Night: %d | [R] Restart", dayCount, nightCount))
+
 	return nil
 }
 
@@ -156,7 +166,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 
 	for _, b := range g.Ball {
-		vector.FillCircle(screen, float32(b.X), float32(b.Y), 10, b.Color, true)
+		vector.FillCircle(screen, float32(b.X), float32(b.Y), 8, b.Color, true)
 	}
 }
 
@@ -165,7 +175,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("Go-pongwars")
 
