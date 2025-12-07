c4a6e2efcba48def13c54d3413a7f52e5825bb3b
VictorDMoura <mouramvictor@gmail.com>
2025-12-07 18:03:32 -0300

    refactor: improve code formatting and add color to balls

--- main.go
+++ main.go
@@ -11,20 +11,21 @@ import (
 const (
 	ScreenWidth  = 800
 	ScreenHeight = 800
-	TileSize = 20
-	Cols = ScreenWidth / TileSize
-	Rows = ScreenHeight / TileSize
+	TileSize     = 20
+	Cols         = ScreenWidth / TileSize
+	Rows         = ScreenHeight / TileSize
 )
 
 var (
-	ColorDay = color.RGBA{R: 200, G: 200, B: 200, A: 255}
+	ColorDay   = color.RGBA{R: 200, G: 200, B: 200, A: 255}
 	ColorNight = color.RGBA{R: 50, G: 50, B: 50, A: 255}
-	ColorBall = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	ColorBall  = color.RGBA{R: 255, G: 0, B: 0, A: 255}
 )
 
 type Ball struct {
-	X, Y float64
+	X, Y   float64
 	DX, DY float64
+	Color  color.Color
 }
 
 type Game struct {
@@ -35,8 +36,8 @@ type Game struct {
 func NewGame() *Game {
 	g := &Game{}
 
-	for x:= 0; x < Cols; x++ {
-		for y:= 0; y < Rows; y++ {
+	for x := 0; x < Cols; x++ {
+		for y := 0; y < Rows; y++ {
 			if x < Cols/2 {
 				g.Grid[x][y] = ColorDay
 			} else {
@@ -45,13 +46,14 @@ func NewGame() *Game {
 		}
 	}
 
-	g.Ball = append(g.Ball, &Ball{X: 200, Y: 400, DX: 4, DY: 4})
-	g.Ball = append(g.Ball, &Ball{X: 600, Y: 400, DX: -4, DY: -4})
+	g.Ball = append(g.Ball, &Ball{X: 200, Y: 400, DX: 4, DY: 4, Color: ColorNight})
+	g.Ball = append(g.Ball, &Ball{X: 600, Y: 400, DX: -4, DY: -4, Color: ColorDay})
 
-	return  g
+	return g
 }
 
 func (g *Game) Update() error {
+
 	for _, b := range g.Ball {
 		b.X += b.DX
 		b.Y += b.DY
@@ -68,23 +70,23 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	for x:= 0; x < Cols; x++ {
-		for y:= 0; y < Rows; y++ {
+	for x := 0; x < Cols; x++ {
+		for y := 0; y < Rows; y++ {
 			vector.FillRect(screen, float32(x*TileSize), float32(y*TileSize), TileSize, TileSize, g.Grid[x][y], false)
 		}
 	}
 
 	for _, b := range g.Ball {
-		vector.FillCircle(screen, float32(b.X), float32(b.Y), 10, ColorBall, true)
+		vector.FillCircle(screen, float32(b.X), float32(b.Y), 10, b.Color, true)
 	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return ScreenWidth, ScreenWidth
+	return ScreenWidth, ScreenHeight
 }
 
-func main(){
-	
+func main() {
+
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("Go-pongwars")
 
